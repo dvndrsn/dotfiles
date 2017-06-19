44ebdf7964a4c0979300afeb8eb59fe3673d163a
Dave A <dave@dvndrsn.com>
2017-06-19 12:06:01 -0400

    Add more git aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,10 +1,13 @@
-
 # Git
 alias gg='git log --oneline --abbrev-commit --all --graph --decorate --color'
 alias gr='git log -g --abbrev-commit --pretty=oneline --date=iso8601'
-alias gst='git status'
-alias gp='git pull --rebase'
+alias gst='git status --short'
+alias ga='git add'
+alias ga.='git add .'
+alias glr='git pull --rebase'
 alias gc='git commit'
+alias gcm='git commit -m'
+alias pg='git push'
 
 # Ruby
 alias be='bundle exec'
